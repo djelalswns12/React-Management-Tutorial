6ce92652757e9f2b1048f14a360ad6befbae672b
Mj Kim <djelalswns12@naver.com>
2021-02-16 02:43:45 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers=[{
+  'id':1,
+  'img': 'https://placeimg.com/64/64/2',
+  'name':'김민준',
+  'birthday':'970808',
+  'gender':'남자',
+  'job':'대학생',
+},
+{
+  'id':2,
+  'img': 'https://placeimg.com/64/64/5',
+  'name':'이승현',
+  'birthday':'990808',
+  'gender':'여자',
+  'job':'고등학생',
+}
+]
 
 function App() {
   return (
-    <div className="gray-background">
-        <img src={logo} lat="lgo"></img>
-        <h2>관리프로그램 만들어보자!!@dddd</h2>
-    </div>
+      customers.map(c=>{
+        return(
+          <Customer
+          key={c.id}
+          id={c.id}
+          name={c.name}
+          img={c.img}
+          birthday={c.birthday}
+          gender={c.gender}
+          job={c.job}
+          />
+        )
+      })
+
   );
 }
 
