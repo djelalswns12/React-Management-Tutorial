15df99a3aea904615e327b317535815e5faf736a
Mj Kim <djelalswns12@naver.com>
2021-02-16 02:05:20 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+        <img src={logo} lat="lgo"></img>
+        <h2>관리프로그램 만들어보자!!@dddd</h2>
+    </div>
+  );
+}
+
+export default App;
